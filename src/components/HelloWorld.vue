af6c7de5cc3a8b0377b45b8ed25f8bb3c0acfbb0
Dawid Skrzypczyk <skrzypmajster@gmail.com>
2022-02-21 23:13:24 +0100

    Install and configure Vuetify 3 components library

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,7 @@ defineProps<{
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
       <a target="_blank" href="https://vuejs.org/">Vue 3</a>. What's next?
+      <v-btn>default</v-btn>
     </h3>
   </div>
 </template>
