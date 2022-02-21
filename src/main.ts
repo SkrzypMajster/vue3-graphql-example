4d25d2f84e08c931a24b2b5f17b41880fc024d0e
Dawid Skrzypczyk <skrzypmajster@gmail.com>
2022-02-22 00:35:53 +0100

    Configure Apollo client instance

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import './main.scss';
 
-import { createApp } from 'vue'
+import {createApp, h, provide} from 'vue'
+import {createVuetify} from "vuetify";
+import {DefaultApolloClient} from "@vue/apollo-composable";
+import {ApolloClient, InMemoryCache} from "@apollo/client/core";
+
 import App from './App.vue'
 import router from './router'
-import {createVuetify} from "vuetify";
 
-const app = createApp(App)
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    cache,
+    uri: process.env.VUE_APP_API_URL,
+})
+
+const app = createApp({
+    setup () {
+        provide(DefaultApolloClient, apolloClient)
+    },
+
+    render: () => h(App),
+})
 
 app.use(router)
 app.use(createVuetify())
